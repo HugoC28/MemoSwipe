151644ed9e9ad170571606e2a77f3530485bd27e
HugoC28 <hugocalio@yahoo.fr>
2023-12-29 23:41:05 +0100

    Styling fix

--- MemoSwipe/src/screens/EventDetailScreen.tsx
+++ MemoSwipe/src/screens/EventDetailScreen.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { View, Button, Text } from 'react-native';
+import { View, Button, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import Icon from 'react-native-ico-material-design';
 import { StackNavigationProp } from '@react-navigation/stack';
 import { RootStackParamList } from '../navigation/AppNavigator';
 import commonStyles from '../assets/styles';
@@ -15,9 +16,33 @@ const EventDetailScreen: React.FC<EventDetailScreenProps> = ({ navigation }) => 
     pickImage({userId: 'user0034'});
   };
 
+  const styles = StyleSheet.create({
+    buttonContainer: {
+      backgroundColor: '#10416D',
+      borderRadius: 15,
+      width: 60,
+      height: 60,
+      alignItems: 'center',
+      justifyContent: 'center',
+      transform: [{ rotate: '45deg' }],
+      shadowColor: '#000',
+      shadowOffset: { width: 0, height: 1 },
+      shadowOpacity: 0.5,
+      shadowRadius: 2,
+      elevation: 5,
+      margin:40,
+    },
+    buttonsIcon: {
+      transform: [{ rotate: '-45deg' }],
+    },
+    button:{
+      borderRadius: 10,
+    }
+  });
+
   return (
     <View style={commonStyles.container}>
-      <View style={{ height: 200, justifyContent: 'center', alignItems: 'center', flexDirection: 'row' }}>
+      <View style={{ width:'100%' , height: 200, marginBottom:4, backgroundColor: '#d9d9d9', justifyContent: 'center', alignItems: 'center', flexDirection: 'row' }}>
         <Button
           title="<"
           onPress={() => navigation.navigate('EventsList')}
@@ -29,19 +54,21 @@ const EventDetailScreen: React.FC<EventDetailScreenProps> = ({ navigation }) => 
         />
       </View>
       <EventImagesOverview eventId='mBRSLWVzup7jEsZyITVn' />
-      <View style={{ height: 100, justifyContent: 'center', alignItems: 'center', flexDirection: 'row' }}>
-        <Button
-          title="Download"
-          onPress={() => navigation.navigate('DownloadAlbum')}
-        />
-        <Button
-          title="+"
-          onPress={handleUploadButtonClick}
-        />
+      <View style={{width:'100%' , height: 100, marginTop:4, backgroundColor: '#d9d9d9', justifyContent: 'center', alignItems: 'center', flexDirection: 'row' }}>
+        <TouchableOpacity onPress={() => navigation.navigate('DownloadAlbum')}>
+          <View style={styles.buttonContainer}>
+            <Icon name="download-button" style={styles.buttonsIcon} width={50} heihgt={50} color="white" />
+          </View>
+        </TouchableOpacity>
         <Button
           title="Evaluate"
           onPress={() => navigation.navigate('EvaluatePhoto')}
         />
+        <TouchableOpacity onPress={handleUploadButtonClick}>
+          <View style={styles.buttonContainer}>
+            <Icon name="add-plus-button" style={styles.buttonsIcon} width={50} heihgt={50} color="white" />
+          </View>
+        </TouchableOpacity>        
       </View>
     </View>
   );
