e7969c09e12eaef3032f6fd85c12309443d56c00
Tim <tim.hosch@gmx.de>
2023-12-30 20:22:35 +0100

    designed home screen

--- MemoSwipe/src/screens/HomeScreen.tsx
+++ MemoSwipe/src/screens/HomeScreen.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native';
+import { View, TouchableOpacity, Text, Image, StyleSheet } from 'react-native';
 import { StackNavigationProp } from '@react-navigation/stack';
 import { RootStackParamList } from '../navigation/AppNavigator';
 import commonStyles from '../assets/styles';
@@ -9,19 +9,73 @@ type HomeScreenProps = {
 };
 
 const HomeScreen: React.FC<HomeScreenProps> = ({ navigation }) => {
-  return (
+  return (    
     <View style={commonStyles.container}>
-      <Text style={commonStyles.text}>Welcome to MemoSwipe !</Text>
-      <Button
-        title="Sign In"
-        onPress={() => navigation.navigate('SignIn')}
+       <Image
+        source={require('../assets/logo.png')}
+        style={styles.logo}
       />
-      <Button
-        title="Log In"
-        onPress={() => navigation.navigate('LogIn')}
-      />
+      <Text style={styles.title}>MemoSwipe</Text>
+      <Text style={styles.subtitle}>Share your memories</Text>
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity
+          style={styles.button}          
+          onPress={() => navigation.navigate("SignIn")}
+        >
+          <Text style={styles.buttontext}>Sign Up</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.button}          
+          onPress={() => navigation.navigate("LogIn")}
+        >
+          <Text style={styles.buttontext}>Login</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  logo: {
+    width: 200,
+    height: 200, 
+    marginBottom: 20,
+  },
+  title: {
+    fontSize: 35,
+    fontWeight: 'bold',
+    marginBottom: 10,
+    color: "#000000"
+  },
+  subtitle: {
+    fontSize: 20,
+    marginBottom: 50,
+    color: "#555555"
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '70%', 
+  },
+  button: {
+    width: 120, 
+    height: 40, 
+    backgroundColor: '#2F80ED',
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 10,
+    textAlign: 'center',
+    borderRadius: 30
+   },
+   buttontext: {
+    color: '#FFFFFF',
+   },
+});
+
 
 export default HomeScreen;
