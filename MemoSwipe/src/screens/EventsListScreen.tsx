659baac7794c96cb04cc7a8a9d03713b0ccb75ee
Tim <tim.hosch@gmx.de>
2024-01-01 18:32:55 +0100

    get user on Eventlist (test)

--- MemoSwipe/src/screens/EventsListScreen.tsx
+++ MemoSwipe/src/screens/EventsListScreen.tsx
@@ -3,15 +3,21 @@ import { View, Button, Text } from 'react-native';
 import { StackNavigationProp } from '@react-navigation/stack';
 import { RootStackParamList } from '../navigation/AppNavigator';
 import commonStyles from '../assets/styles';
+import { getUserId, getUserEmail, getUsername } from '../services/authService';
 
 type EventsListScreenProps = {
     navigation: StackNavigationProp<RootStackParamList, 'EventsList'>;
   };
 
 const EventsListScreen: React.FC<EventsListScreenProps> = ({navigation}) => {
+
+
   return (
     <View style={commonStyles.container}>
       <Text>My Events</Text>
+      <Text>UserID: {getUserId()}</Text>
+      <Text>Username: {getUsername()}</Text>
+      <Text>UserEmail {getUserEmail()}</Text>
       <Button
         title="+"
         onPress={() => navigation.navigate('CreateEvent')}
