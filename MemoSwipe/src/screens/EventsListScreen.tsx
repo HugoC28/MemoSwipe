b8c06756f109665ba9f4e8f6046fd1b126927393
Tim <tim.hosch@gmx.de>
2024-01-01 21:23:51 +0100

    finished design

--- MemoSwipe/src/screens/EventsListScreen.tsx
+++ MemoSwipe/src/screens/EventsListScreen.tsx
@@ -1,12 +1,13 @@
 import React, { useEffect, useState } from 'react';
-import { View, Image,TouchableOpacity, Text, FlatList, Button, StyleSheet } from 'react-native';
+import { View, Image,TouchableOpacity, ActivityIndicator, Text, FlatList, Button, StyleSheet } from 'react-native';
 import { StackNavigationProp } from '@react-navigation/stack';
 import { RootStackParamList } from '../navigation/AppNavigator';
 import commonStyles from '../assets/styles';
 import firestore from '@react-native-firebase/firestore';
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
-import { faCamera, faUserGroup, faAngleRight} from '@fortawesome/free-solid-svg-icons';
+import { faCamera, faUserGroup, faAngleRight, faPlus, faRotateRight} from '@fortawesome/free-solid-svg-icons';
 import { getUserId, getUserEmail, getUsername } from '../services/authService';
+import { color } from '@rneui/themed/dist/config';
 
 interface Event {
   id: string;
@@ -25,40 +26,44 @@ const EventsListScreen: React.FC<EventsListScreenProps> = ({navigation}) => {
   const userId = getUserId();
 
   const [events, setEvents] = useState<Event[]>([]);
+  const [loading, setLoading] = useState(true);
+  const fetchEvents = async () => {
+    setLoading(true);
+    try {
+      const snapshot = await firestore()
+        .collection('Events')
+        .where('members_id', 'array-contains', userId)
+        .get();
+      const eventData: Event[] = await Promise.all(
+          snapshot.docs.map(async (doc) => {
+            const photosSnapshot = await doc.ref.collection('Photos').get();
+            const photosDocCount = photosSnapshot.size;
   
-    useEffect(() => {
-      const fetchEvents = async () => {
-        try {
-          const snapshot = await firestore()
-            .collection('Events')
-            .where('members_id', 'array-contains', userId)
-            .get();
-          const eventData: Event[] = await Promise.all(
-              snapshot.docs.map(async (doc) => {
-                const photosSnapshot = await doc.ref.collection('Photos').get();
-                const photosDocCount = photosSnapshot.size;
-      
-                return {
-                  id: doc.id,
-                  title: doc.get('title'),
-                  description: doc.get('description'),
-                  memberCount: (doc.get('members_id') as any[]).length,
-                  photoCount: photosDocCount,
-                };
-              })
-            );
-            setEvents(eventData);
-        } catch (error) {
-          console.error('Error fetching events:', error);
-        }
-      };
-  
-      fetchEvents();
-    }, []);
+            return {
+              id: doc.id,
+              title: doc.get('title'),
+              description: doc.get('description'),
+              memberCount: (doc.get('members_id') as any[]).length,
+              photoCount: photosDocCount,
+            };
+          })
+        );
+        setEvents(eventData);
+    } catch (error) {
+      console.error('Error fetching events:', error);
+    }
+    finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {    
+    fetchEvents();
+  }, []);
 
 
   return (
-    <View style={commonStyles.container}>
+    <View style={styles.container}>
       <View style={styles.headerView}>     
         <View style={styles.headerContainer}>
           <Image
@@ -66,62 +71,80 @@ const EventsListScreen: React.FC<EventsListScreenProps> = ({navigation}) => {
           style={styles.logo}
           />
           <Text style={styles.appname}>MemoSwipe</Text>  
-        </View>   
-        <Text style={styles.title}>My Events</Text>             
+        </View> 
+        <View style={styles.titleContainer}>
+        <TouchableOpacity onPress={() => fetchEvents()}>
+          <FontAwesomeIcon icon={faRotateRight} style={styles.headerIcons} size={25} />
+        </TouchableOpacity>
+        <Text style={styles.title}>My Events</Text>
+        <TouchableOpacity onPress={() => navigation.navigate('CreateEvent')}>
+          <FontAwesomeIcon icon={faPlus} style={styles.headerIcons} size={25} />
+        </TouchableOpacity>
+        </View>            
       </View>
-      <Text>UserID: {getUserId()}</Text>
-      <Text>Username: {getUsername()}</Text>
-      <Text>UserEmail {getUserEmail()}</Text>
+      {loading ? (
+        <ActivityIndicator style={styles.activityIndicator} size={50} color={'#2F80ED'}/>
+      ) : (
       <FlatList
         style={styles.list}
         data={events}
         keyExtractor={(item) => item.id}
         renderItem={({ item }) => (
-          <View style={styles.listItem}>
-            <TouchableOpacity onPress={() => navigation.navigate('EventDetail', {eventId: item.id})}>
-            <View style={styles.listTextContainer}>              
-              <Text style={styles.listTitle}>{item.title}</Text>
-              <Text style={styles.listDescription}> {item.description}</Text>
+          <TouchableOpacity onPress={() => navigation.navigate('EventDetail', {eventId: item.id})}>
+            <View style={styles.listItem}>
+            
+              <View style={styles.listTextContainer}>              
+                <Text style={styles.listTitle}>{item.title}</Text>
+                <Text style={styles.listDescription}> {item.description}</Text>
+              </View>
+              <View style={styles.listIconContainer}> 
+                <FontAwesomeIcon icon={faCamera} style={styles.listIcons} size={25} />
+                <Text>{item.photoCount}</Text>
+                <FontAwesomeIcon icon={faUserGroup} style={styles.listIcons} size={25} />
+                <Text>{item.memberCount}</Text>
+                <FontAwesomeIcon icon={faAngleRight} style={styles.listRightArrow} size={25} />
+              </View>            
             </View>
-            <View style={styles.listIconContainer}> 
-            <FontAwesomeIcon icon={faCamera} style={styles.listIcons} size={25} />
-            <Text>{item.photoCount}</Text>
-            <FontAwesomeIcon icon={faUserGroup} style={styles.listIcons} size={25} />
-            <Text>{item.memberCount}</Text>
-            <FontAwesomeIcon icon={faAngleRight} style={styles.listRightArrow} size={25} />
-            </View>
-            </TouchableOpacity>
-          </View>
+          </TouchableOpacity>
         )}
-      />
-      <Button
-        title="+"
-        onPress={() => navigation.navigate('CreateEvent')}
-      />
+      />)}
     </View>
   );
 };
 
 const styles = StyleSheet.create({
+  container:{
+    flex: 1,
+    flexDirection: "column",
+    justifyContent: "flex-start",
+  },
   headerView: {
     padding: 10,
-    backgroundColor: "#ffff",    
+    backgroundColor: "#E8EEF3",    
     width: '100%',
     alignItems: 'center',  
   },
   headerContainer: {
-    flexDirection: 'row',
-    backgroundColor: "#0F0",   
+    flexDirection: 'row',   
     alignItems: 'center',    
     width: "100%",
-    justifyContent: "flex-start",
+    //justifyContent: "flex-start",
+    justifyContent: "center",
+  },
+  titleContainer: {
+    flexDirection: 'row',  
+    alignItems: 'center',    
+    width: "100%",
+    justifyContent: "space-between",
+    padding: 10,
   },
   title: {
     fontSize: 24,
     fontWeight: 'bold',
-    marginTop: 10,
-    marginBottom: 5,
     color: "#000000"
+  },
+  headerIcons: {   
+    color: "#2F80ED",
   },
   logo: {
     width: 50 ,
@@ -137,41 +160,43 @@ const styles = StyleSheet.create({
   },
   listItem: {
     padding: 10,
-    backgroundColor: "#888",    
+    borderBottomColor: 'black',
+    borderBottomWidth: StyleSheet.hairlineWidth,   
     width: '100%',
     flexDirection: 'row',
     alignItems: 'center',    
     justifyContent: "space-between",
   },
-  listTextContainer: {
-    backgroundColor: "#333",    
+  listTextContainer: {   
     justifyContent: "flex-start",
   },
-  listIconContainer: {
-    backgroundColor: "#F00",   
+  listIconContainer: {   
     alignItems: 'center',
     flexDirection: "row",
-    justifyContent: "flex-start",
+    justifyContent: "flex-end",
   },
   listTitle: {        
     fontSize: 20,
-    //textAlign: "left",    
+    textAlign: "left",    
     fontWeight: 'bold',
     marginTop: 5,
+    color: "#343C44",
   },
   listDescription: {        
     fontSize: 12,
-    textAlign: "left",   
-    marginBottom: 5,
+    textAlign: "left", 
   },
   listIcons: {   
-    color: "#999999",
+    color: "#343C44",
     marginRight: 5,
     marginLeft: 5,
   },
   listRightArrow: {   
-    color: "#999999",   
+    color: "#AAA",   
     marginLeft: 15,
+  },
+  activityIndicator:{
+    padding: 20
   },
   
 });
